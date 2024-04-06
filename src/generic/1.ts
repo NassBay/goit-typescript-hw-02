db012435d5eeb04bf86acbeb23b7000f56177835
Anastasiia Baidina <107297045+NassBay@users.noreply.github.com>
2024-04-06 12:20:12 +0300

    Update 1.ts

--- src/generic/1.ts
+++ src/generic/1.ts
@@ -3,12 +3,11 @@
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
 */
 
-function getPromise<T extends (string | number)[]>() {
+function getPromise<T extends (string | number)[]>(): Promise<T> {
   return new Promise((resolve) => {
     resolve(['Text', 50] as T);
   });
 }
-
 getPromise()
   .then((data) => {
     console.log(data); 
